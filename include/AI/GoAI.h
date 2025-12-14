763faf56338c9655377320b24bace4ac4ad4a6de
khanh47 <phhkhanh47@gmail.com>
2025-12-14 19:00:57 +0700

    lower time limit of bot from 5s to 3s

--- include/AI/GoAI.h
+++ include/AI/GoAI.h
@@ -45,7 +45,7 @@ private:
 	static constexpr int dy[4] = {0, 1, 0, -1};
 
 private:
-	static constexpr int MAX_TIME_MS = 5000;
+	static constexpr int MAX_TIME_MS = 3000;
 	std::chrono::high_resolution_clock::time_point _timeLimit;
 	bool _timeOut = false;
 	void startTimer();
